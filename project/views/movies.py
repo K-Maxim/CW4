e5a135fed6128a10f05fc21307553a4fdb186373
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-06-08 14:46:59 +0300

    Вьюхи

---
+++ project/views/movies.py
@@ -0,0 +1,28 @@
+from flask_restx import abort, Namespace, Resource
+
+from project.exceptions import ItemNotFound
+from project.services.movie_service import MovieService
+
+from project.setup_db import db
+
+movies_ns = Namespace("movies")
+
+
+@movies_ns.route("/")
+class MovieView(Resource):
+    @movies_ns.response(200, "OK")
+    def get(self):
+        """Get all movies"""
+        return MovieService(db.session).get_all_movies()
+
+
+@movies_ns.route("/<int:movie_id>")
+class MovieView(Resource):
+    @movies_ns.response(200, "OK")
+    @movies_ns.response(404, "Movie not found")
+    def get(self, movie_id: int):
+        """Get movie by id"""
+        try:
+            return MovieService(db.session).get_item_by_id(movie_id)
+        except ItemNotFound:
+            abort(404, message="Director not found")
---
+++ project/views/users.py
@@ -0,0 +1,64 @@
+from flask import request
+from flask_restx import abort, Namespace, Resource
+
+from project.exceptions import ItemNotFound
+from project.services.user_service import UsersService
+
+from project.setup_db import db
+
+
+users_ns = Namespace("users")
+
+
+@users_ns.route("/")
+class UsersView(Resource):
+
+    @users_ns.response(200, "OK")
+    def get(self):
+        """Get all users"""
+        return UsersService(db.session).get_all_users()
+
+
+@users_ns.route("/<int:user_id>")
+class UserView(Resource):
+
+    @users_ns.response(200, "OK")
+    @users_ns.response(404, "User не найден")
+    def get(self, user_id: int):
+        """Get user by id"""
+        try:
+            return UsersService(db.session).get_item_by_id(user_id)
+        except ItemNotFound:
+            abort(404, message="User `не найден`")
+
+    def patch(self, user_id: int):
+        req_json = request.json
+        if not req_json:
+            abort(400)
+        if not req_json.get("id"):
+            req_json['id'] = user_id
+        try:
+            return UsersService(db.session).update(req_json)
+        except ItemNotFound:
+            abort(404)
+
+
+@users_ns.route("/password/<int:user_id>")
+class UserPatchView(Resource):
+
+    @users_ns.response(200, "OK")
+    @users_ns.response(404, "User не найден")
+    def put(self, user_id: int):
+        req_json = request.json
+        password_1 = req_json.get("password_1", None)
+        password_2 = req_json.get("password_2", None)
+        if None in [password_1, password_2]:
+            abort(400)
+        if not password_1 or not password_2:
+            abort(400)
+        if not req_json.get("id"):
+            req_json['id'] = user_id
+        try:
+            return UsersService(db.session).update_password(req_json)
+        except ItemNotFound:
+            abort(404)
