c0b94220c387c3bb73f4fc8447cef1449647dee0
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-06-08 14:47:14 +0300

    основной код программы

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from project.config import DevelopmentConfig
 from project.dao.models import Genre
+from project.dao.models.director import Director
+from project.dao.models.movie import Movie
+from project.dao.models.user import User
 from project.server import create_app, db
 
 app = create_app(DevelopmentConfig)
@@ -10,4 +13,11 @@ def shell():
     return {
         "db": db,
         "Genre": Genre,
+        "User": User,
+        "Director": Director,
+        "Movie": Movie,
     }
+
+
+if __name__ == '__main__':
+    app.run(port='25000')
