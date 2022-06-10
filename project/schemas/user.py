486357283c57bb10a452ddceaddc18ec722c3afc
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-06-10 21:09:08 +0300

    Update user.py

--- project/schemas/user.py
+++ project/schemas/user.py
@@ -4,8 +4,8 @@ from marshmallow import fields, Schema
 class UserSchema(Schema):
     id = fields.Int(required=True)
     email = fields.Str(required=True)
-    password = fields.Str()
-    name = fields.Str()
-    surname = fields.Str()
-    favorite_genre = fields.Str()
+    password = fields.Str(required=True)
+    name = fields.Str(required=True)
+    surname = fields.Str(required=True)
+    favorite_genre = fields.Str(required=True)
 
